bdd65cd5b139520e4266038dbedace0adef45976
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2022-09-30 12:57:17 +0300

    Added local storage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import styles from './Navigation.module.css'
 
 const Navigation = () => {
     return (
-        <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
+        <nav className={styles.navbar}>
             <h3 className="font-bold">Currency search</h3>
             <span>
                 <Link to="/" className="mr-2">Home</Link>
--- src/store/currency/favSlice.js
+++ src/store/currency/favSlice.js
@@ -1,16 +1,20 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+const FAV_KEY = 'cfk'
+
 const favSlice = createSlice({
     name: 'fav',
     initialState: {
-        fav:[]
+        fav: JSON.parse(localStorage.getItem(FAV_KEY) ?? '[]')
     },
     reducers: {
         addToFav(state, action) {
             state.fav.push(action.payload)
+            localStorage.setItem(FAV_KEY, JSON.stringify(state.fav))
         },
         deleteFav(state, action) {
             state.fav = state.fav.filter(item => item.txt !== action.payload)
+            localStorage.setItem(FAV_KEY, JSON.stringify(state.fav))
         }
     }
 })
