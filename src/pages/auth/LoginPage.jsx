5cba726134999f3fa704e3c44fb094fa0b314167
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2022-10-04 23:35:01 +0300

    added styles from registration/login page

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import Login from "../../components/Login";
 
 const LoginPage = () => {
     return (
         <div className="text-center">
-            <h1>Login</h1>
                 <Login/>
-            <p> Or <Link to="/register">register</Link></p>
         </div>
     );
 };
